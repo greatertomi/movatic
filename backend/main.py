83eea0e5c2f9e60c37c5caa01583cd9f315a3e76
Oshalusi John <oshalusijohn@gmail.com>
2021-03-06 17:49:15 +0100

    Reorganized file in preparation for dockerization

---
+++ backend/main.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_restful import Api, Resource
+import requests
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+api = Api(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+class Stations(Resource):
+    @cross_origin()
+    def get(self):
+        req = requests.get('https://gbfs.bcycle.com/bcycle_madison/station_information.json')
+        return req.json()
+
+
+class StationsStatus(Resource):
+    
+    @cross_origin()
+    def get(self):
+        req = requests.get('https://gbfs.bcycle.com/bcycle_madison/station_status.json')
+        return req.json()
+
+
+api.add_resource(Stations, '/stations')
+api.add_resource(StationsStatus, '/stationsStatus')
+
+if __name__ == "__main__":
+    app.run(debug=True)
